f1b827c1599d8c38cccca1a0113b9d771e491a8d
hrishi508 <k.hrishi2010@gmail.com>
2023-07-02 14:21:49 +0530

    Update app icon and splash screen

--- app/src/main/java/com/example/aichatbot/MainActivity.java
+++ app/src/main/java/com/example/aichatbot/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.aichatbot;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.splashscreen.SplashScreen;
 
 import android.os.Bundle;
 
@@ -8,6 +9,9 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // Handle the splash screen transition.
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
